dfa9c1da6c05c6b059383cd78f1c258b6d37a556
Nestor <nestor.bergar@gmail.com>
2026-06-03 23:50:34 +0200

    condicionales a medio hacer

---
+++ 08_condicionales.py
@@ -0,0 +1,19 @@
+### Condicionales ###
+
+my_condition = False
+
+if my_condition: #? Si no se cumple la condición no se ejecuta el if, normalmente un true
+    print("Se ejecuta la condición del if")
+
+my_condition = 5*3
+
+if my_condition == 10:
+    print("Se ejecuta la condición del segundo if")
+
+if my_condition > 10 and my_condition < 20:
+    print("es mayor que 10 y menor que 20")
+else:
+    print("es menor o igual que 10 o mayor o igual que 20")
+
+print("La ejecución continúa")
+
