4e9d00857eb939f46dbe96906f3caa162cf277ec
Nesttorbg <nestor.bergar@gmail.com>
2026-06-08 11:02:21 +0200

    Condicionales terminados

--- 08_condicionales.py
+++ 08_condicionales.py
@@ -12,8 +12,20 @@ if my_condition == 10:
 
 if my_condition > 10 and my_condition < 20:
     print("es mayor que 10 y menor que 20")
+elif my_condition == 1:
+        print("es igual a 1")
 else:
     print("es menor o igual que 10 o mayor o igual que 20")
 
+
+print("es menor o igual que 10 o mayor o igual que 20")
+
 print("La ejecución continúa")
 
+my_string = "Mi cadena de texto"
+
+if not my_string:
+    print("My cadena de texto no es vacía") 
+
+if my_string == ("Mi cadena de textoo"):
+    print("Estas cadenas de texto coinciden")  
